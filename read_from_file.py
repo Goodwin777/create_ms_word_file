6c0435541a8c8ec8a15c58b34c4a0c025d23ce5b
AlexejHavrylov <al.gavrylov@gmail.com>
2019-03-14 20:45:14 +0200

    Added module that take all text from file

--- read_from_file.py
+++ read_from_file.py
@@ -1,8 +1,16 @@
-import os
-
 class ReadFromFile:
     def read_file(self):
+        all_text = []
         with open('song_list.txt','r') as file:
-            line = file.readline()
+            # title = file.readline()[0]
+            for i in file.readlines():
+                all_text.append(i)
 
-            return line
+
+
+            return all_text
+
+
+if __name__ == '__main__':
+    obj = ReadFromFile()
+    print obj.read_file()
